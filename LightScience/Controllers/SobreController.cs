5e2b9abb5ca583aabd2e836ac6dd7fda45e14206
Lucas Campos <lucas.cgoncalves7@gmail.com>
2024-05-05 20:14:24 -0300

    inclusão landing page

---
+++ LightScience/Controllers/SobreController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LightScence.Controllers
+{
+	public class SobreController : Controller
+	{
+		[Authorize]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
